a6b270806a774c37336a9da258e7526c4c72fbb7
Henry Au <hau@cpp.edu>
2017-02-20 23:39:25 -0800

    p2

--- 05-Color-Game/game.js
+++ 05-Color-Game/game.js
@@ -8,8 +8,9 @@ var colors = [
 ];
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
@@ -23,9 +24,24 @@ for(var i = 0; i < squares.length; i++) {
         var clickedColor = this.style.background;
         //compare color to pickedColor
         if(clickedColor === pickedColor) {
-            alert("Correct!");
+            messageDisplay.textContent = "Correct!";
+            changeColors(clickedColor);
         } else {
-            alert("Wrong..");
+            this.style.background = "#232323";
+            messageDisplay.textContent = "Try Again";
         }
     });
+}
+
+function changeColors(color) {
+    // change each color to match given color
+    for(var i = 0; i < squares.length; i++) {
+        squares[i].style.background = color;
+    }
+}
+
+function pickColor() {
+    // random number up to colors.length + .999... Example: 6.999
+    Math.floor(Math.random() * colors.length);
+    return colors[random];
 }
