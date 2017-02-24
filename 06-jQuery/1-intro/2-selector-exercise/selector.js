03975e3131fa86d19641d454e919f50d84db0267
Henry Au <hau@cpp.edu>
2017-02-23 23:29:13 -0800

    jquery selectors

---
+++ 06-jQuery/1-intro/2-selector-exercise/selector.js
@@ -0,0 +1,11 @@
+$("div").css("background", "purple");
+
+// Select all div with class highlight
+$("div.highlight").css("width", "200px");
+
+// Select div with id of third
+$("div#third").css("border", "2px solid orange");
+
+$("div:first-of-type").css("color", "pink");
+// jquery shortcut - slower
+// $("div:first").css("color", "pink");
