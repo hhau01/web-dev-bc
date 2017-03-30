cc61c011e499342e749e8e48ebc5eb3dee18bbe3
Henry Au <henryau01@gmail.com>
2017-03-29 21:07:53 -0700

    attr and val, & manipulating classes

---
+++ 06-jQuery/1-intro/3-methods/methods-demo.js
@@ -0,0 +1,32 @@
+$('img').css('width', '250px');
+
+// change to terrifying pine marten
+// $('img').attr('src', 'https://c3.staticflickr.com/3/2418/2243463214_f32ab004af_b.jpg');
+
+$('input').attr('type', 'color');
+
+$('input').attr('type', 'checkbox');
+
+$('input').attr('type', 'text');
+
+$('img:first-of-type').attr('src', 'https://c3.staticflickr.com/3/2418/2243463214_f32ab004af_b.jpg');
+
+$('img').last().attr('src', 'https://c3.staticflickr.com/3/2418/2243463214_f32ab004af_b.jpg');
+
+$('input').val("henry");
+
+$('input').val('');
+
+console.log($('select').val());
+
+$('h1').addClass('correct');
+
+$('li').addClass('wrong');
+
+$('li').toggleClass('wrong');
+
+$('li').toggleClass('wrong');
+
+$('li').first().toggleClass('done');
+
+$('li').toggleClass('done');
