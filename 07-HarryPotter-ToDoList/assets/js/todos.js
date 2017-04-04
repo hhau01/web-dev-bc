1cf87c5570a7a3101b59575ccc7b3e636b35c373
Henry Au <henryau01@gmail.com>
2017-04-03 21:38:12 -0700

    create todos

--- 07-HarryPotter-ToDoList/assets/js/todos.js
+++ 07-HarryPotter-ToDoList/assets/js/todos.js
@@ -1,4 +1,20 @@
 // check off todos by clicking
-$('li').click(function() {
+$('ul').on('click', 'li', function() {
     $(this).toggleClass('completed');
+});
+
+// click X to delete todo
+$('ul').on('click', 'span', function(event) {
+    $(this).parent().fadeOut(300, function() {
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event) {
+    if (event.which === 13) {
+        var todoText = $(this).val();
+        $(this).val("");
+        $('ul').append('<li><span>X</span> ' + todoText + '</li>');
+    }
 });
