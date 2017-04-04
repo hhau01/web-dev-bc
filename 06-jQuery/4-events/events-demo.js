a725239d26a60627a3d082ff4ab9428a054a8231
Henry Au <henryau01@gmail.com>
2017-04-03 17:56:20 -0700

    jquery events

--- 06-jQuery/4-events/events-demo.js
+++ 06-jQuery/4-events/events-demo.js
@@ -1,7 +1,32 @@
-$("h1").click(function() {
-    alert('h1 clicked');
+// $("h1").click(function() {
+//     alert('h1 clicked');
+// });
+
+// $("button").click(function() {
+//     var text = $(this).text();
+//     console.log("You clicked " + text);
+//     $(this).css("background", "pink");
+// });
+
+// $("input[type='text'").keypress(function(event) {
+//     if (event.which === 13) {
+//         alert("You hit ENTER")
+//     }
+// });
+
+$('h1').on('click', function() {
+    $(this).css('color', 'purple')
 });
 
-$("button").click(function() {
-    $(this).css("background", "pink");
+$('input').on('keypress', function() {
+    console.log('Key pressed');
 });
+
+$('button').on('mouseenter', function() {
+    $(this).css('font-weight', 'bold');
+});
+
+$('button').on('mouseleave', function() {
+    $(this).css('font-weight', 'normal');
+});
+
